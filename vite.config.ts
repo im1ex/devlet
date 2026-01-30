196c1a1ee3d126d885f029bb3d17febe4986b954
im1ex <lex.lar@gmail.com>
2026-01-30 23:38:09 +0100

    Set up dependencies and vite PWA plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,31 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    VitePWA(),
+
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   workbox: {
+    //     globPatterns: ["**/*"],
+    //   },
+    //   includeAssets: [
+    //     "**/*",
+    //   ],
+    //   manifest: {
+    //     name: 'Developer Wallet',
+    //     short_name: 'DevLet',
+    //     description: 'A EVM wallet for developers',
+    //     // ToDo: add icons
+    //     // icons: [],
+
+    //     start_url: '/',
+    //     scope: '/',
+
+    //     theme_color: '#fdbefb',
+    //   },
+    //   devOptions: {
+    //     enabled: true
+    //   }
+    // }),
   ],
 })
